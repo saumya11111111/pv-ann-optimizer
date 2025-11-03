27e51a422d5dbe4265a06352552155e418c8cb87
saumya11111111 <153817772+saumya11111111@users.noreply.github.com>
2025-11-04 02:48:34 +0530

    pv parameters adjusted to Jaisalmer

--- GenerateInputs.m
+++ GenerateInputs.m
@@ -1,8 +1,8 @@
 function [irradiance, temperature] = GenerateInputs()
 %#codegen
 
-% Random irradiance between 600–1200 W/m²
-irradiance = 600 + 600 * rand;
+% Random irradiance between 600–1200 W/m² with clamping 
+irradiance = max(600 + 200 * randn, 100);
 
 % Random panel surface temperature between 50–75 °C
 temperature = 50 + 25 * rand;
